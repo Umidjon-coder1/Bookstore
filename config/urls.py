b9d726896c57d9c43f00f7fb75b3af433bfad95e
Elshodbek Turayev <elshodbekturayev2005@gmail.com>
2026-06-11 17:35:23 +0300

    Serve media files in production (fix image display)

--- config/urls.py
+++ config/urls.py
@@ -53,6 +53,6 @@ urlpatterns = [
     path('api/redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
 ]
 
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
     urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
